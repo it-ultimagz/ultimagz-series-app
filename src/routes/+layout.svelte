884ec7813b9275a2a70ab0cb2d3c3ee5b3033ffc
adrianfinantyo <adrianfinantyo@gmail.com>
2023-10-05 21:28:23 +0700

    feat: create nav for mobile and desktop

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,58 @@
 <script lang="ts">
   import "../app.scss";
-  let header: HTMLElement;
+  import Fa from "svelte-fa";
+  import { faBars, faBook, faBookmark, faEarthAmericas, faGlobe } from "@fortawesome/free-solid-svg-icons";
+  import { faInstagram, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
 
-  document.addEventListener("scroll", () => {
-    const classList = ["bg-white", "shadow-md", "border-b-2", "border-red-600"];
-    if (window.scrollY > 0) {
-      header.classList.add(...classList);
-    } else {
-      header.classList.remove(...classList);
-    }
-  });
+  let header: HTMLElement;
+  let isListenerAdded = false;
+
+  $: if (!isListenerAdded) {
+    document.addEventListener("scroll", () => {
+      const classList = ["bg-white", "shadow-md", "border-b-2", "border-red-600"];
+      if (window.scrollY > 0 && header) {
+        header.classList.add(...classList);
+      } else if (header) {
+        header.classList.remove(...classList);
+      }
+    });
+    isListenerAdded = true;
+  }
 </script>
 
 <header class="fixed top-0 left-0 w-full z-50 transition-all duration-100" bind:this={header}>
-  <nav class="mx-auto container flex justify-between py-5">
+  <nav class="desktop mx-auto container flex justify-center md:justify-between py-5 items-center">
     <a href="/">
-      <h1 class="font-bold text-2xl">ULTIMAGZ</h1>
+      <h1 class="brand font-black text-3xl bg-gradient-to-r from-red-500 to-red-900 bg-clip-text text-transparent">
+        ULTIMAGZ
+      </h1>
     </a>
-    <ul class="decoration-none flex gap-4">
-      <li><a class="nav-item" href="https://ultimagz.com" target="_blank">MAGZ</a></li>
-      <li><a class="nav-item" href="https://fokus.ultimagz.com" target="_blank">FOKUS</a></li>
-      <li><a class="nav-item" href="https://pdf.ultimagz.com" target="_blank">PDF</a></li>
+    <ul class="hidden md:flex list-none gap-4">
+      <li><a href="https://ultimagz.com" target="_blank">MAGZ</a></li>
+      <li><a href="https://fokus.ultimagz.com" target="_blank">FOKUS</a></li>
+      <li><a href="https://pdf.ultimagz.com" target="_blank">PDF</a></li>
+    </ul>
+  </nav>
+  <nav class="mobile mx-auto w-screen container fixed bottom-[20px] left-0 block md:hidden">
+    <ul class="bg-white w-full h-full py-3 border-2 border-slate-400 rounded-xl flex justify-evenly">
+      <li>
+        <a href="https://ultimagz.com" target="_blank">
+          <Fa icon={faEarthAmericas} class="text-red-600 text-xl" />
+          <span class="text-xs">MAGZ</span>
+        </a>
+      </li>
+      <li>
+        <a href="https://fokus.ultimagz.com" target="_blank">
+          <Fa icon={faBookmark} class="text-red-600 text-xl" />
+          <span class="text-xs">FOKUS</span>
+        </a>
+      </li>
+      <li>
+        <a href="https://pdf.ultimagz.com" target="_blank">
+          <Fa icon={faBook} class="text-red-600 text-xl" />
+          <span class="text-xs">PDF</span>
+        </a>
+      </li>
     </ul>
   </nav>
 </header>
@@ -30,10 +62,41 @@
 </main>
 
 <footer class="bg-slate-800">
-  <div class="mx-auto container py-10 text-white">
-    <div>
-      <h1 class="text-3xl font-bold">ULTIMAGZ</h1>
+  <div class="mx-auto container py-20 text-white flex justify-between items-center">
+    <div class="space-y-2">
+      <a href="https://ultimagz.com">
+        <h1
+          class="text-4xl font-black bg-gradient-to-r from-red-500 to-red-900 bg-clip-text hover:text-transparent transition-all duration-100"
+        >
+          ULTIMAGZ
+        </h1>
+      </a>
       <p class="text-md">&copy; 2023 ULTIMAGZ. All rights reserved.</p>
+    </div>
+    <div class="space-y-2">
+      <h3>Contact Us On</h3>
+      <ul class="flex gap-4">
+        <li>
+          <a href="https://ultimagz.com">
+            <Fa icon={faGlobe} class="text-white text-xl" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/ultimagz">
+            <Fa icon={faInstagram} class="text-white text-xl" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/channel/UCu-eKnze0d6gbX9cRMp9SRA">
+            <Fa icon={faYoutube} class="text-white text-xl" />
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/ultimagz">
+            <Fa icon={faTwitter} class="text-white text-xl" />
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 </footer>
